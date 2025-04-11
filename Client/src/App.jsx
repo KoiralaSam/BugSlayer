c50b7d1c5bbf1e7a1ddc6f7b5b8ea71d802de860
Samarpan Koirala <samarpankoirala@gmail.com>
2025-04-11 13:45:07 -0500

    Shop component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,16 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
-import "./App.css"
-
-import LandingPage from "./routes/LandingPage"
+import "./App.css";
+import Shop from "./routes/shop";
+import LandingPage from "./routes/LandingPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/shop" element={<Shop />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
