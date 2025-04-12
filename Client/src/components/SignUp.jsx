63296a37981ddab319d31f3db118bf5995ac854f
adk-saugat <adhikarisaugat34@gmail.com>
2025-04-11 23:58:34 -0500

    dummy 2nd

--- Client/src/components/SignUp.jsx
+++ Client/src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import { FaXmark } from "react-icons/fa6"
 
 function SignUp({ setShowSignUp }) {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-[rgb(0,0,0,.5)] flex justify-center items-center">
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-[rgb(0,0,0,.5)] flex justify-center items-center z-10">
       <div className="w-[500px] h-[600px] bg-white rounded-2xl p-6">
         <div className="flex justify-end">
           <FaXmark
