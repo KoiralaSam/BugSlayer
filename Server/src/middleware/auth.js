1d80fb9e487f989b3f91b99d442a4d122732f02a
KoiralaSam <samarpankoirala@gmail.com>
2025-04-12 13:19:27 -0500

    fixed json

--- Server/src/middleware/auth.js
+++ Server/src/middleware/auth.js
@@ -4,21 +4,24 @@ import jwt from "jsonwebtoken";
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
+    console.log("Token received in auth middleware:", token); // Debugging
     const decoded = jwt.verify(token, process.env.JWT_SIGN_KEY);
-
+    console.log("Decoded token:", decoded); // Debugging
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
     });
 
     if (!user) {
-      throw new Error("Could not authenticate");
+      throw new Error("Authentication failed");
     }
+
     req.token = token;
     req.user = user;
     next();
-  } catch (e) {
-    res.status(401).send(e);
+  } catch (error) {
+    console.error("Error in auth middleware:", error.message); // Debugging
+    res.status(401).send({ error: "Please authenticate." });
   }
 };
 
